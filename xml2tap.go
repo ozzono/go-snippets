d36cc3f0093e654b2aeadded613b25be2021ac33
Hugo <hugo@beneficiofacil.com.br>
2020-03-05 14:37:20 -0300

    Added adb tap method using parsed coords

--- xml2tap.go
+++ xml2tap.go
@@ -3,34 +3,37 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"os/exec"
 	"strconv"
 	"strings"
 )
 
-var input string
+var (
+	input string
+	tap   bool
+)
 
 func init() {
 	flag.StringVar(&input, "i", "", "Sets the xml styled input")
+	flag.BoolVar(&tap, "tap", false, "Enables adb tap for inputed coords")
 }
-
 
 func main() {
 	flag.Parse()
 	if len(input) > 0 {
-		if strings.Contains(input, ",") {
-			for _, item := range strings.Split(input, "|") {
-				xml2tap(item)
-			}
+		xml2tap(input)
+		x, y := xml2tap(input)
+		fmt.Println("teste")
+		if tap && x != 0 && y != 0 {
+			shell(fmt.Sprintf("adb shell input tap %d %d", x, y))
 			return
 		}
-		xml2tap(input)
-		return
 	}
-	xml2tap("[90,1204][990,1303]")
-	xml2tap("[90,1225][376,1281]")
 }
 
-func xml2tap(xmlcoords string) {
+func xml2tap(xmlcoords string) (int, int) {
+	fmt.Println("Parsing coords")
 	openbracket := "["
 	closebracket := "]"
 	joinedbracket := "]["
@@ -40,22 +43,45 @@ func xml2tap(xmlcoords string) {
 		rightcoords := strings.Split(string(stringcoords[1][:len(stringcoords[1])-1]), ",")
 		x1, err := strconv.Atoi(leftcoords[0])
 		if err != nil {
-			return
+			fmt.Printf("atoi err: %v", err)
+			return 0, 0
 		}
 		y1, err := strconv.Atoi(leftcoords[1])
 		if err != nil {
-			return
+			fmt.Printf("atoi err: %v", err)
+			return 0, 0
 		}
 		x2, err := strconv.Atoi(rightcoords[0])
 		if err != nil {
-			return
+			fmt.Printf("atoi err: %v", err)
+			return 0, 0
 		}
 		y2, err := strconv.Atoi(rightcoords[1])
 		if err != nil {
-			return
+			fmt.Printf("atoi err: %v", err)
+			return 0, 0
 		}
 		x := (x1 + x2) / 2
 		y := (y1 + y2) / 2
 		fmt.Printf("%s --- x: %d y: %d\n", xmlcoords, x, y)
+		return x, y
 	}
+	return 0, 0
+}
+
+func shell(arg string) string {
+	fmt.Println("shell: " + arg)
+	args := strings.Split(arg, " ")
+	if len(args) == 1 {
+		args = append(args, "")
+	} else if len(args) < 1 {
+		log.Println("Invalid command")
+		return ""
+	}
+	out, err := exec.Command(args[0], args[1:]...).CombinedOutput()
+	if err != nil {
+		log.Printf("Command: '%v'; Output: %v; Error: %v\n", arg, string(out), err)
+		return err.Error()
+	}
+	return string(out)
 }

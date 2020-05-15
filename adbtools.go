324dbfeb2fc02dfea3a1d4120a38697b626aedfe
Hugo Virgilio <hugo.virgilio@beneficiofacil.com.br>
2020-05-15 08:46:32 -0300

    Many fixes to package functioning

--- adbtools.go
+++ adbtools.go
@@ -13,18 +13,26 @@ var (
 	loglvl   bool
 )
 
+type device struct {
+	ID string
+}
+
 // TODO: Validate the need of the given commands
 // free memory verification and storage: adb shell cat /proc/meminfo |grep MemFree
 
 // Shell executes the given command in the Linux bash terminal
 // and return the command output as string
-func Shell(arg string) string {
-	if len(deviceID) > 0 {
-		arg = strings.Replace(arg, "adb", fmt.Sprintf("adb -s %s", deviceID), -1)
+func (device *device) Shell(arg string) string {
+	if len(device.ID) > 0 {
+		arg = strings.Replace(arg, "adb", fmt.Sprintf("adb -s %s", device.ID), -1)
 	}
 	if loglvl {
-		log.Println("adb shell: " + arg)
+		log.Println(arg)
 	}
+	return shell(arg)
+}
+
+func shell(arg string) string {
 	args := strings.Split(arg, " ")
 	out, err := exec.Command(args[0], args[1:]...).Output()
 	if err != nil {
@@ -37,18 +45,15 @@ func Shell(arg string) string {
 	return string(out)
 }
 
-// IsActive verifies if the given package is on foreground
-func IsActive(appPackage string) bool {
-	// TODO: futurually add string normalization
-	if strings.Contains(strings.ToLower(Shell("adb shell dumpsys window windows|grep Focus")), strings.ToLower(appPackage)) {
-		return true
-	}
-	return false
+// Foreground verifies if the given package is on foreground
+func (device *device) Foreground(appPackage string) bool {
+	// TODO: futurally add string normalization
+	return strings.Contains(strings.ToLower(device.Shell("adb shell dumpsys window windows|grep Focus")), strings.ToLower(appPackage))
 }
 
 // TapScreen taps the given coords and waits the given delay in Milliseconds
-func TapScreen(x, y, delay int) {
-	Shell(fmt.Sprintf("adb shell input tap %d %d", x, y))
+func (device *device) TapScreen(x, y, delay int) {
+	device.Shell(fmt.Sprintf("adb shell input tap %d %d", x, y))
 	sleep(delay)
 	return
 }
@@ -59,43 +64,38 @@ func sleep(delay int) {
 }
 
 // XMLScreen fetches the screen xml data
-func XMLScreen(newdump bool) string {
+func (device *device) XMLScreen(newdump bool) string {
 	if newdump {
-		Shell("adb shell uiautomator dump")
+		device.Shell("adb shell uiautomator dump")
 	}
-	return Shell("adb shell cat /sdcard/window_dump.xml")
+	return device.Shell("adb shell cat /sdcard/window_dump.xml")
 }
 
 // TapCleanInput tap and cleans the input
-func TapCleanInput(x, y, charcount int) {
+func (device *device) TapCleanInput(x, y, charcount int) {
 	charcount = charcount/2 + 1
-	TapScreen(x, y, 0)
-	Shell("adb shell input keyevent KEYCODE_MOVE_END")
+	devices.TapScreen(x, y, 0)
+	device.Shell("adb shell input keyevent KEYCODE_MOVE_END")
 	for i := 0; i < charcount; i++ {
-		Shell(`adb shell input keyevent --longpress $(printf 'KEYCODE_DEL %.0s' {1..2})`)
+		device.Shell(`adb shell input keyevent --longpress $(printf 'KEYCODE_DEL %.0s' {1..2})`)
 	}
 }
 
-func Swipe(coords [4]int) {
-	Shell(fmt.Sprintf("adb shell input swipe %d %d %d %d", coords[0], coords[1], coords[2], coords[3]))
+func (device *device) Swipe(coords [4]int) {
+	device.Shell(fmt.Sprintf("adb shell input swipe %d %d %d %d", coords[0], coords[1], coords[2], coords[3]))
 }
 
-func CloseApp(app string) {
-	Shell(fmt.Sprintf("adb shell am force-stop %s", app))
+func (device *device) CloseApp(app string) {
+	device.Shell(fmt.Sprintf("adb shell am force-stop %s", app))
 }
 
 // ClearApp clears all the app data
-func ClearApp(app string) error {
-	output := Shell(fmt.Sprintf("adb shell pm clear %s", app))
+func (device *device) ClearApp(app string) error {
+	output := device.Shell(fmt.Sprintf("adb shell pm clear %s", app))
 	if strings.Contains(output, "Success") {
 		return nil
 	}
 	return fmt.Errorf("Failed to clear %s app data. Output: %s", app, output)
-}
-
-// DeviceID sets the device Id to be used by the adb
-func DeviceID(dID string) {
-	deviceID = dID
 }
 
 // Loglvl enables the logging of every shell command
@@ -103,7 +103,7 @@ func Loglvl(verbose bool) {
 	loglvl = verbose
 }
 
-func InputText(text string, splitted bool) error {
+func (device *device) InputText(text string, splitted bool) error {
 	if len(text) == 0 {
 		return fmt.Errorf("invalid input; cannot be empty")
 	}
@@ -111,20 +111,34 @@ func InputText(text string, splitted bool) error {
 	text = strings.Replace(text, " ", "\\s", -1)
 	if splitted {
 		for _, textRune := range strings.Split(text, "") {
-			Shell(fmt.Sprintf("adb shell input text %v", textRune))
+			device.Shell(fmt.Sprintf("adb shell input text %v", textRune))
 		}
 		return nil
 	}
-	Shell("adb shell input text %s" + text)
+	device.Shell("adb shell input text %s" + text)
 	return nil
 }
 
-func PageDown() {
+func (device *device) PageDown() {
 	// code 93 is equivalent to "KEYCODE_PAGE_DOWN"
-	Shell("adb shell input keyevent 93")
+	device.Shell("adb shell input keyevent 93")
 }
 
-func PageUp() {
+func (device *device) PageUp() {
 	// code 92 is equivalent to "KEYCODE_PAGE_UP"
-	Shell("adb shell input keyevent 92")
+	device.Shell("adb shell input keyevent 92")
+}
+
+func Devices() []device {
+	output := []device{}
+	for _, row := range strings.Split(device.Shell("adb devices"), "\n") {
+		if strings.Contains(row, "device") {
+			output = append(output, Device{ID: strings.Split(row, "	")[0]})
+		}
+	}
+	return output
+}
+
+func NewDevice(deviceID string) device {
+	return device{ID: deviceID}
 }

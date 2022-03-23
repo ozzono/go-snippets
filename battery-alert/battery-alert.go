597501b6a628b8864827635fa01abc8adb05bfee
Hugo Virgílio <huvirgilio@gmail.com>
2022-03-22 22:42:10 -0300

    Fortune Commit: I own seven-eighths of all the artists in downtown Burbank!

---
+++ battery-alert/battery-alert.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"math"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/0xAX/notificator"
+	"github.com/distatus/battery"
+)
+
+const (
+	minThreshHold = 20
+	maxThreshHold = 90
+)
+
+var logPath string
+
+func init() {
+	flag.StringVar(&logPath, "log-path", "", "log file path")
+}
+
+func main() {
+	flag.Parse()
+	b, err := battery.GetAll()
+	if err != nil {
+		log.Panic(err)
+	}
+	btrWatch(*b[0])
+}
+
+func btrWatch(b battery.Battery) {
+	log.Println("main")
+	log.Println("starting battery watcher")
+	for {
+		btrlvl := math.Round(b.Current / b.Full * 100)
+		state := strings.ToLower(b.State.String())
+
+		var notify = notificator.New(notificator.Options{
+			AppName: "Battery Alert",
+		})
+		if state == "discharging" && btrlvl < minThreshHold {
+			notify.Push("Battery Alert", fmt.Sprintf("Discharding battery has reached %.2f%s", btrlvl, "%"), "", notificator.UR_CRITICAL)
+			writeToFile(fmt.Sprintf("%s battery level alert: %.2f%s\n", logPrepend(), btrlvl, "%"))
+		}
+		if state == "charging" && btrlvl > maxThreshHold {
+			notify.Push("Battery Alert", fmt.Sprintf("Discharding battery has reached %.2f%s", btrlvl, "%"), "", notificator.UR_CRITICAL)
+			writeToFile(fmt.Sprintf("%s battery level alert: %.2f%s\n", logPrepend(), btrlvl, "%"))
+		}
+		time.Sleep(time.Minute)
+	}
+}
+
+func logPrepend() string {
+	return time.Now().Format("2006-01-02 15:04:05")
+}
+
+func writeToFile(data string) error {
+	if logPath == "" {
+		return nil
+	}
+	file, err := os.OpenFile(logPath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = io.WriteString(file, data)
+	if err != nil {
+		return err
+	}
+	return file.Sync()
+}
---
+++ battery-alert/go.mod
@@ -0,0 +1,10 @@
+module battery-alert
+
+go 1.17
+
+require (
+	github.com/0xAX/notificator v0.0.0-20220220101646-ee9b8921e557 // indirect
+	github.com/distatus/battery v0.10.0 // indirect
+	golang.org/x/sys v0.0.0-20220319134239-a9b59b0215f8 // indirect
+	howett.net/plist v1.0.0 // indirect
+)
